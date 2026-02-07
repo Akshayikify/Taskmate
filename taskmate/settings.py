192b03d3942375076e4a814c10ce11455c689a65
Akshay H <sumams758@gmail.com>
2026-02-07 23:39:38 +0530

    Settings file updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -76,7 +76,8 @@ WSGI_APPLICATION = 'taskmate.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
